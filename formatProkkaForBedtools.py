4e0033c652bf8f828378b143c99ac356378ae517
Deepika Gunasekaran <deepika.gunasekaran.15@gmail.com>
2016-05-20 15:27:19 -0700

    Format prokka script with all bugs fixed

--- formatProkkaForBedtools.py
+++ formatProkkaForBedtools.py
@@ -9,17 +9,18 @@ def fileModify(query):
     return_line = ''
     for i in query:
         line_list = i.split('\t')
-        line_list_pre = line_list[:3]
-        line_list_suf = line_list[5:8]
-        line_last = line_list[8]
-        line = 'Synteny\t' + str(line_list[3]) + '\t' + str(line_list[4]) + '\t'
-        for j in line_list_pre:
-            line = line + str(j) + '\t'
-        for k in line_list_suf:
-            line = line + str(k) + '\t'
-        line += line_last
-        return_line += line
-        #return_line += '\n'
+        if len(line_list) == 9:
+            line_list_pre = line_list[:3]
+            line_list_suf = line_list[5:8]
+            line_last = line_list[8]
+            line = 'Synteny\t' + str(line_list[3]) + '\t' + str(line_list[4]) + '\t'
+            for j in line_list_pre:
+                line = line + str(j) + '\t'
+            for k in line_list_suf:
+                line = line + str(k) + '\t'
+            line += line_last
+            return_line += line
+            #return_line += '\n'
     return return_line
 
 prokka_clean = open(sys.argv[1], 'r')
@@ -29,7 +30,7 @@ for line in prokka_clean:
     prokka_gff.append(line)
     
 mod_prokka = fileModify(prokka_gff)
-print mod_prokka
+#print mod_prokka
 output_prokka.write(mod_prokka)
 prokka_clean.close()
 output_prokka.close()
