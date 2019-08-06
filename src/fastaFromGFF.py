d9a15d7aaa5acd48b8fb9900d265a865e6c65719
Sarah Ramirez-Busby <smramire@gmail.com>
2019-08-06 14:32:55 -0700

    Removing right-most newline character in cases where the gene feature is the last item in the feature
    Since each feature in a GFF is on a single line, the gene name could
    be at the end of the feature, which would have a newline character.
    This is now stripped.

--- src/fastaFromGFF.py
+++ src/fastaFromGFF.py
@@ -23,7 +23,7 @@ def create_fasta(directory):
             for line in raw_out:
                 gff_id = gff_name + '-' + [j.split('=')[1] for i in line.split('\t')
                                            if i.startswith('ID=') for j in i.split(';')][0]
-                gene = [i.split('=')[1] for i in line.split(';') if i.startswith('gene=')][0]
+                gene = [i.split('=')[1].rstrip('\n') for i in line.split(';') if i.startswith('gene=')][0]
                 gff_gene_dict[gff_id] = gene
                 translation = [i.split('=')[1] for i in line.split(';') if i.startswith('translation=')][0]
                 record = SeqRecord(Seq(translation.rstrip('\n')),
