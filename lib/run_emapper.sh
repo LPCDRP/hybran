a77c983acecc978ae56fab11a6f60252f029b390
Sarah Ramirez-Busby <smramire@gmail.com>
2019-08-07 10:06:34 -0700

    Created a separate function to verify installations and added functionality for anaconda installation
    Moved verifying RATT_HOME and eggnog-mapper databases to a separate
    function termed verify_installations().
    Since eggnog-mapper will be a dependency for anaconda upload, verifying
    the executable was not necessary. However, verifying the presence of the
    downloaded databases is still required.

--- lib/run_emapper.sh
+++ lib/run_emapper.sh
@@ -3,12 +3,25 @@
 nproc=$1
 emapper_loc=$2
 
-cwd=$(pwd)
+emapper.py 				\ 
+	--data_dir $emapper_loc 	\ 
+	-i eggnog_seqs.fasta 		\ 
+	--output mtb_diamond 		\ 
+	-m diamond 			\ 
+	--go_evidence experimental 	\ 
+	--seed_ortholog_evalue 0.000001 \ 
+	--report_orthologs 		\ 
+	--cpu $nproc
 
-cd $emapper_loc
-./emapper.py -i eggnog_seqs.fasta --output mtb_diamond -m diamond --go_evidence experimental --seed_ortholog_evalue 0.000001 --report_orthologs --cpu $nproc
-
-./emapper.py -i eggnog_seqs.fasta --output mtb_hmm -m hmmer -d bact --hmm_evalue 0.000001 --go_evidence experimental --seed_ortholog_evalue 0.000001 --report_orthologs --cpu $nproc
-
-cd $cwd
+emapper.py 				\ 
+	--data_dir $emapper_loc 	\ 
+	-i eggnog_seqs.fasta 		\ 
+	--output mtb_hmm 		\ 
+	-m hmmer 			\ 
+	-d bact 			\ 
+	--hmm_evalue 0.000001 		\ 
+	--go_evidence experimental 	\ 
+	--seed_ortholog_evalue 0.000001 \ 
+	--report_orthologs 		\ 
+	--cpu $nproc
 
