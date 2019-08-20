583d308220b4f91aa8b747fa5fbee8335c30a031
Sarah Ramirez-Busby <smramire@gmail.com>
2019-08-20 09:22:01 -0700

    Updated run_emapper.sh to use the bactNOG and added tax_scope parameter
    emapper.py requires the tax_scope parameter to be flagged to annotate
    all COGs. This was added to both diamond and hmmer executions. Also,
    the bactNOG database is now being used.

--- lib/run_emapper.sh
+++ lib/run_emapper.sh
@@ -4,6 +4,7 @@ nproc=$1
 emapper_loc=$2
 
 emapper.py 				\ 
+	--tax_scope bactNOG		\ 
 	--data_dir $emapper_loc 	\ 
 	-i eggnog_seqs.fasta 		\ 
 	--output mtb_diamond 		\ 
@@ -14,11 +15,12 @@ emapper.py 				\
 	--cpu $nproc
 
 emapper.py 				\ 
+	--tax_scope bactNOG		\ 
 	--data_dir $emapper_loc 	\ 
 	-i eggnog_seqs.fasta 		\ 
 	--output mtb_hmm 		\ 
 	-m hmmer 			\ 
-	-d bact 			\ 
+	-d bactNOG 			\ 
 	--hmm_evalue 0.000001 		\ 
 	--go_evidence experimental 	\ 
 	--seed_ortholog_evalue 0.000001 \ 
