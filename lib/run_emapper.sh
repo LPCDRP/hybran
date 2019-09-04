268cca94424600f7059236d5a6fcc327e672dbe4
Sarah Ramirez-Busby <smramire@gmail.com>
2019-09-04 08:15:06 -0700

    Created a directory for eggnog-mapper output
    Added a mkdir to the run_eggnog_mapper() that creates a directory for all
    eggnog output

--- lib/run_emapper.sh
+++ lib/run_emapper.sh
@@ -3,27 +3,7 @@
 nproc=$1
 emapper_loc=$2
 
-emapper.py 				\ 
-	--tax_scope bactNOG		\ 
-	--data_dir $emapper_loc 	\ 
-	-i eggnog_seqs.fasta 		\ 
-	--output mtb_diamond 		\ 
-	-m diamond 			\ 
-	--go_evidence experimental 	\ 
-	--seed_ortholog_evalue 0.000001 \ 
-	--report_orthologs 		\ 
-	--cpu $nproc
+emapper.py --tax_scope bactNOG --data_dir $emapper_loc -i eggnog_seqs.fasta --output eggnog-mapper-annotations/mtb_diamond -m diamond --go_evidence experimental --seed_ortholog_evalue 0.000001 --report_orthologs --cpu $nproc
 
-emapper.py 				\ 
-	--tax_scope bactNOG		\ 
-	--data_dir $emapper_loc 	\ 
-	-i eggnog_seqs.fasta 		\ 
-	--output mtb_hmm 		\ 
-	-m hmmer 			\ 
-	-d bactNOG 			\ 
-	--hmm_evalue 0.000001 		\ 
-	--go_evidence experimental 	\ 
-	--seed_ortholog_evalue 0.000001 \ 
-	--report_orthologs 		\ 
-	--cpu $nproc
+emapper.py --tax_scope bactNOG --data_dir $emapper_loc -i eggnog_seqs.fasta --output eggnog-mapper-annotations/mtb_hmm -m hmmer -d bactNOG --hmm_evalue 0.000001 --go_evidence experimental  --seed_ortholog_evalue 0.000001 --report_orthologs --cpu $nproc
 
