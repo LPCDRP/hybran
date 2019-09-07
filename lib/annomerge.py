db73ddfc16870897f4b8946f2b4630bbff869485
Sarah Radecke <sramirezbusby@sdsu.edu>
2019-09-07 08:10:14 -0700

    Instead of file_path using annotation_fp for prodigal test results
    file_path includes the file name of the genome being annotated.
    Prodigal results are output in the main annotation directory. This was
    updated

--- lib/annomerge.py
+++ lib/annomerge.py
@@ -1538,7 +1538,7 @@ def run(isolate_id, annotation_fp, ref_proteins_fasta, ref_embl_fp, reference_ge
     incorrect_coords_dict = {}
     run_prodigal(reference_genome)
 
-    prodigal_results_fp = file_path + 'prodigal-test/reference_prodigal'
+    prodigal_results_fp = annotation_fp + '/prodigal-test/reference_prodigal'
     dict_save_fp = prodigal_results_fp + 'reference_prodigal.p'
     ref_embl_record = SeqIO.read(ref_embl_fp, 'embl')
     prodigal_raw = open(prodigal_results_fp, "r").readlines()
