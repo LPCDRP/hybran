7b68d308f7923b6e91dedc9668fe05d445917098
Sarah Radecke <sramirezbusby@sdsu.edu>
2019-09-07 07:32:58 -0700

    Moved verification of installations to a module
    To reduce lines in the main script, the function that verifies the
    installations was moved to a firstReference.py module

---
+++ lib/verifyInstallations.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+
+def verify_installations(eggnog_databases):
+    """
+
+    :param eggnog_databases:
+    :return:
+    """
+    logger = logging.getLogger('VerifyInstallations')
+
+    # Verify the RATT_HOME variable is set
+    if not os.environ['RATT_HOME']:
+        logger.error('RATT_HOME variable is not defined. '
+                     'Please export RATT_HOME=[location of downloaded ratt executable/config')
+        exit(EnvironmentError)
+    # Verify eggnog-mapper/ is present in given directory
+    if not os.path.isdir(eggnog_databases):
+        logger.error('There are no databases in ' + eggnog_databases + '. Location of eggnog databases is required.'
+                                                                       '\nIf the databases have not been downloaded, '
+                                                                       'execute download_eggnog_data.py and follow '
+                                                                       'prompts. The Diamond AND HMM databases are '
+                                                                       'required.')
+        exit(OSError)
