abeadd787c7d07a3ec79f228a3184d7616a01ced
Afif Elghraoui <aelghraoui@sdsu.edu>
2021-10-27 15:32:00 -0700

    remove duplicate code for running CDHIT
    parseClustering.ref_seqs has a complete copy of the code from CDHIT.py
    with two subtle differences:
    
    - ref_seqs calls CD-HIT with -c 0.99 and -A 1.0. It appears to me that
      they intended 100% alignment coverage, but `-A` is a
      coverage *control* option and goes by absolute difference, so in
      this case a difference of one amino acid. In any case, CDHIT.py
      takes alignment coverage (set via -aL and -aS) and sequence identity
      as options, so this isn't a problem. We simply call this function
      instead, but set both seq_ident and seq_covg to 99%.
    - CDHIT.py's version of `create_reps_fasta` did not include sequence
      descriptions in the fasta file, while ref_seqs' did, so I've added
      that extra line of code into CDHIT.py
    
    the only remaining changes in CDHIT.py are the adding of docstrings
    where missing and updating them where I noticed anything outdated.

--- src/hybran/CDHIT.py
+++ src/hybran/CDHIT.py
@@ -5,13 +5,22 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 def create_allseq_dict(fa):
-    """Parses FASTA input, creates dictionary with sequence ID as key and the sequence as the value"""
+    """
+    Parses FASTA input, creates dictionary with sequence ID as key and the sequence as the value
+
+    :param fa: str fasta file name
+    :returns:
+         - fasta_dict (:py:class:`dict`) protein sequence dict by ID
+         - fasta_descrip (:py:class:`dict`) sequence descriptions by ID
+    """
     fasta_dict={}
+    fasta_descrip={}
     for record in SeqIO.parse(fa, "fasta"):
         sequence = str(record.seq)
         fasta_dict[record.id] = sequence
-    return fasta_dict
-    
+        fasta_descrip[record.id] = record.description
+    return fasta_dict, fasta_descrip
+
 
 def run_cdhit(nproc, input, output, seq_ident, seq_covg):
     """Runs cdhit.
@@ -37,8 +46,15 @@ def run_cdhit(nproc, input, output, seq_ident, seq_covg):
 
 
 def create_reps_dict(in_clusters):
-    """Creates a dictionary with the representative sequence ID from each cluster as the key
-    and the corresponding sequences as the value."""
+    """
+    Creates a dictionary with the representative sequence ID from each cluster as the key
+    and the corresponding sequences as the value.
+
+    :param in_clusters: str file name of clusters
+    :returns:
+         - rep_dict (:py:class:`dict`) cluster members by representatives
+         - reps (:py:class:`list`) representatives
+    """
     clusters = []
     rep_dict = {}
     rep = ''
@@ -64,22 +80,42 @@ def create_reps_dict(in_clusters):
     return rep_dict, reps
 
 
-def create_reps_fasta(output, reps, rep_seq_id_dict):
-    """Creates a FASTA file that has sequence ID and sequence for each representative"""
+def create_reps_fasta(output, reps, rep_seq_id_dict, description_dict):
+    """
+    Creates a FASTA file that has sequence ID
+    and sequence for each representative
+
+    :param output: str output file name
+    :param reps: list of representatives
+    :param rep_seq_id_dict: dict of representatives and sequences
+    :return: None
+    """
     seq_list = []
     for id_key in reps:
-        record = SeqRecord(Seq(rep_seq_id_dict[id_key]), id=id_key, description="")
+        record = SeqRecord(Seq(rep_seq_id_dict[id_key]),
+                           id=id_key,
+                           description=description_dict[id_key])
         seq_list.append(record)
     SeqIO.write(seq_list, output, "fasta")
 
 
 def run(nproc, fasta, seq_ident,seq_covg, out):
+    """
+    Executes CDHIT
+
+    :param nproc: int number of processors
+    :param fasta: str FASTA file name
+    :param seq_ident: float minimum sequence identity
+    :param seq_covg: float minimum alignment coverage
+    :param out: str output file name
+    :return: dict clusters by representative
+    """
     logger = logging.getLogger('CDHIT')
     logger.info('Running CDHIT')
     run_cdhit(nproc, fasta, out, seq_ident, seq_covg)
-    OGdict = create_allseq_dict(fasta)
+    OGdict, description_dict = create_allseq_dict(fasta)
     logger.info('Parsing CDHIT output (' + out + '.clstr)')
     REPdict, rep_list = create_reps_dict(out + '.clstr')
     logger.info('Creating FASTA of CDHIT clusters representatives')
-    create_reps_fasta(out, rep_list, OGdict)
+    create_reps_fasta(out, rep_list, OGdict, description_dict)
     return REPdict
--- src/hybran/parseClustering.py
+++ src/hybran/parseClustering.py
@@ -9,6 +9,8 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from collections import OrderedDict
 from Bio.Blast.Applications import NcbiblastpCommandline
+
+from . import CDHIT
 from . import config
 
 
@@ -346,110 +348,6 @@ def ref_seqs(gbk_dir):
     :return: str FASTA file name and a dictionary of dictionaries
     """
     hybran_tmp_dir = config.hybran_tmp_dir
-    def create_allseq_dict(fa):
-        """
-        Parses FASTA input, creates dictionary with
-        sequence ID as key and the sequence as the value
-
-        :param fa: FASTA file name
-        :return: dict of record IDs and sequences
-        """
-        fasta_dict = {}
-        fasta_descrip = {}
-        for record in SeqIO.parse(fa, "fasta"):
-            sequence = str(record.seq)
-            fasta_dict[record.id] = sequence
-            fasta_descrip[record.id] = record.description
-        return fasta_dict, fasta_descrip
-
-    def run_cdhit(nproc, input, output):
-        """
-        Runs cdhit with a sequence identity threshold of 0.99.
-
-        CDHIT outputs a fasta file of representative sequences (ref_cdss_protein.fasta)
-        and a text file of list of clusters (ref_cdss_protein.fasta.clstr).
-
-        :param nproc: str number of processors to use
-        :param input: str input file name
-        :param output: str output file name
-        :return: subprocess.stdout
-        """
-        cmd = ['cd-hit',
-               '-i', input,
-               '-o', output,
-               '-c', '0.99',
-               '-T', str(nproc),
-               '-g', '1',
-               '-s', '1.0',
-               '-d', '256',
-               '-A', '1.0']
-        out = subprocess.run(cmd, stdout=subprocess.PIPE)
-        return out
-
-    def create_reps_dict(in_clusters):
-        """
-        Creates a dictionary with the representative
-        sequence ID from each cluster as the key
-        and the corresponding sequences as the value.
-
-        :param  in_clusters: str file name of clusters
-        :return: dict of representatives and the CDHIT clusters and a list of representatives
-        """
-        clusters = []
-        rep_dict = {}
-        rep = ''
-        reps = []
-        with open(in_clusters, 'r') as f:
-            for line in f:
-                # At new cluster
-                if line.startswith('>'):
-                    if not rep:
-                        continue
-                    rep_dict[rep] = clusters
-                    clusters = []
-                    continue
-                # Representatives of cluster
-                else:
-                    if line.rstrip('\n').endswith('*'):
-                        rep = line.rstrip('\n').split()[2].replace('>', '').replace('...', '')
-                        reps.append(rep)
-                    else:
-                        gene = line.rstrip('\n').split()[2].replace('>', '').replace('...', '')
-                        clusters.append(gene)
-        rep_dict[rep] = clusters
-        return rep_dict, reps
-
-    def create_reps_fasta(output, reps, rep_seq_id_dict, description_dict):
-        """
-        Creates a FASTA file that has sequence ID
-        and sequence for each representative
-
-        :param output: str output file name
-        :param reps: list of representatives
-        :param rep_seq_id_dict: dict of representatives and sequences
-        :return: None
-        """
-        seq_list = []
-        for id_key in reps:
-            record = SeqRecord(Seq(rep_seq_id_dict[id_key]), id=id_key, description=description_dict[id_key])
-            seq_list.append(record)
-        SeqIO.write(seq_list, output, "fasta")
-
-    def cd_hit(nproc, fasta, out):
-        """
-        Executes CDHIT
-
-        :param nproc: str number of processors
-        :param fasta: str FASTA file name
-        :param out: str output file name
-        :return: None
-        """
-        logger = logging.getLogger('CDHIT')
-        logger.debug('Running CDHIT on reference annotations')
-        cdhit_stdout = run_cdhit(nproc, fasta, out)
-        OGdict, description_dict = create_allseq_dict(fasta)
-        REPdict, rep_list = create_reps_dict(out + ".clstr")
-        create_reps_fasta(out, rep_list, OGdict, description_dict)
 
     def grep_seqs(gff):
         """
@@ -500,7 +398,11 @@ def ref_seqs(gbk_dir):
     with open(all_proteins, 'w') as ref_cds_out:
         for s in protein_cds:
             SeqIO.write(s, ref_cds_out, 'fasta')
-    cd_hit(1, all_proteins, final_proteins)
+    # Run CD-HIT with 99% thresholds
+    CDHIT.run(nproc=1,
+              fasta=all_proteins,
+              seq_ident=99, seq_covg=99,
+              out=final_proteins)
     return final_proteins, isolate_seqs
 
 
