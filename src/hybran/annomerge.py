b13879409e5e035bdfc672c5588d1a9f9c056d5c
Afif Elghraoui <aelghraoui@sdsu.edu>
2023-02-20 20:45:22 -0800

    process_split_genes: check for gene name definition before doing coord_check

--- src/hybran/annomerge.py
+++ src/hybran/annomerge.py
@@ -416,7 +416,8 @@ def process_split_genes(flist):
                 if not designator.is_pseudo(feature.qualifiers) and not designator.is_pseudo(last_gene.qualifiers):
                     continue
             # At least one copy is complete (has consistent-with-reference start *and* stop codons) -- do not combine
-            if ((all(coord_check(last_gene)) and all(coord_check(feature)))
+            if ((('gene' in last_gene.qualifiers and all(coord_check(last_gene)))
+                 and ('gene' in feature.qualifiers and all(coord_check(feature))))
                 and not overlap_inframe(last_gene, feature)):
                 continue
 
