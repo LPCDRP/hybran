LPCDRP/hybran

commit a7aa1f24b2d94846a7afe67cf7c2ce625cb9901f

Set up prodigal correction to work with potentially multiple contig references
Using SeqIO.read() assumes that the reference strain has only a single
sequence/contig, which is not the case when there are plasmids.
Since SeqIO.parse() is already being used to collect the reference CDSs,
most of the information we need can be loaded as part of that process.

src/hybran/annomerge.py
@@ -1332,16 +1332,16 @@ def correct_start_coords_prokka(prokka_record, correction_dict, fasta_seq, rv_se
     return modified_prokka_record
 
 
-def get_feature_by_locustag(rec):
+def get_feature_by_locustag(features):
     """
 
-    :param rec:
+    :param features:
     :return:
     """
     global ref_genes_positions
     ref_genes_positions = {}
     cds_dict = {}
-    for feature in rec.features:
+    for feature in features:
         if feature.type != 'CDS':
             continue
         else:
@@ -1463,54 +1463,60 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_embl_fp, ref
     prokka_rejects_logfile = os.path.join(isolate_id, 'annomerge', 'prokka_unused.tsv')
     annomerge_records = []
 
+    ref_contigs = []
+    ref_features = []
     # create a dictionary of reference CDS annotations (needed for liftover to ab initio)
     ref_annotation = {}
+    # nested dictionary of contig to genomic coordinates to locus tags (needed for prodigal correction)
+    ref_loci = collections.OrderedDict()
     for ref_record in SeqIO.parse(ref_embl_fp, 'embl'):
+        ref_loci[ref_record.id] = collections.OrderedDict()
+        ref_contigs.append(ref_record.seq)
+        ref_features.append(ref_record.features)
         for feature in ref_record.features:
             if feature.type != "CDS":
                 continue
+            ref_loci[ref_record.id][(int(feature.location.start) + 1, int(feature.location.end))] = feature.qualifiers['locus_tag'][0]
             # if reference paralogs have been collapsed, the last occurrence in the genome
             # will prevail.
             ref_annotation[feature.qualifiers['gene'][0]] = feature
 
     # RUNNING PRODIGAL
-    embl_dict = {}
-    prodigal_list = []
+    prodigal_list = {ref_contig:[] for ref_contig in ref_loci.keys()}
     incorrect_coords_dict = {}
     prodigal_results_fp = os.path.join('prodigal-test', 'reference_prodigal')
     if not os.path.exists(prodigal_results_fp):
         run_prodigal(reference_genome, prodigal_results_fp)
 
     dict_save_fp = prodigal_results_fp + '.p'
-    ref_embl_record = SeqIO.read(ref_embl_fp, 'embl')
     prodigal_raw = open(prodigal_results_fp, "r").readlines()
-    for feat in ref_embl_record.features:
-        if feat.type != 'CDS':
-            continue
-        embl_dict[(int(feat.location.start) + 1, int(feat.location.end))] = feat.qualifiers['locus_tag'][0]
-    ordered_embl_dict = collections.OrderedDict(sorted(embl_dict.items()))
     for line in prodigal_raw:
-        if line.startswith('     CDS'):
+        if line.startswith('DEFINITION'):
+            header_fields = re.sub(r'^DEFINITION\s+', '', line.strip())
+            header_fields = dict(_.split('=') for _ in header_fields.replace('"','').split(';'))
+            ref_contig = header_fields['seqhdr']
+        elif line.startswith('     CDS'):
             line_elements = line.strip().split()
             coords = line_elements[1]
             if '(' in coords:
                 coords_elements = coords.split('..')
                 coords_start = coords_elements[0].split('(')[1]
                 coords_end = coords_elements[1].split(')')[0]
-                prodigal_list.append((int(coords_start), int(coords_end)))
+                prodigal_list[ref_contig].append((int(coords_start), int(coords_end)))
             else:
                 coords_elements = coords.split('..')
-                prodigal_list.append((int(coords_elements[0].replace('<', '')), int(coords_elements[1])))
-    for gene_coord in prodigal_list:
-        for gene in ordered_embl_dict.keys():
-            if gene[0] > gene_coord[1]:
-                break
-            elif gene[1] == gene_coord[1] and gene[0] != gene_coord[0]:
-                prodigal_length = gene_coord[1] - gene_coord[0] + 1
-                start_change = gene[0] - gene_coord[0] - 1
-                incorrect_coords_dict[ordered_embl_dict[gene]] = {'length': prodigal_length,
-                                                                  'start_change': start_change}
-                break
+                prodigal_list[ref_contig].append((int(coords_elements[0].replace('<', '')), int(coords_elements[1])))
+    for ref_contig in prodigal_list.keys():
+        for gene_coord in prodigal_list[ref_contig]:
+            for gene in ref_loci[ref_contig].keys():
+                if gene[0] > gene_coord[1]:
+                    break
+                elif gene[1] == gene_coord[1] and gene[0] != gene_coord[0]:
+                    prodigal_length = gene_coord[1] - gene_coord[0] + 1
+                    start_change = gene[0] - gene_coord[0] - 1
+                    incorrect_coords_dict[ref_loci[ref_contig][gene]] = {'length': prodigal_length,
+                                                                         'start_change': start_change}
+                    break
     logger.debug('Number of genes with incorrect start predictions by Prodigal: ' +
                  str(len(list(incorrect_coords_dict.keys()))))
     pickle.dump(incorrect_coords_dict, open(dict_save_fp, "wb"))
@@ -1525,15 +1531,6 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_embl_fp, ref
                              str(incorrect_coords_dict[key]['start_change'])]),
                   file=f)
 
-    prodigal_correction_dict = incorrect_coords_dict
-    ref_feature_dict = get_feature_by_locustag(ref_embl_record)
-    global ref_sequence
-    ref_sequence = ref_embl_record.seq
-    ref_features = ref_embl_record.features
-
-    global ref_prom_fp_dict
-    ref_prom_fp_dict = get_prom_for_gene(ref_features, ref_sequence)
-
     if ratt_enforce_thresholds:
         ratt_seq_ident = seq_ident
         ratt_seq_covg = seq_covg
@@ -1544,11 +1541,17 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_embl_fp, ref
 
     for i, contig in enumerate(contigs):
         seqname = '.'.join([isolate_id, contig])
+        global ref_sequence
+        ref_sequence = ref_contigs[i]
+        ref_feature_dict = get_feature_by_locustag(ref_features[i])
         ratt_contig_record = SeqIO.read(ratt_gbk_files[contig], 'genbank')
         global record_sequence
         record_sequence = ratt_contig_record.seq
         prokka_contig_record_pre = prokka_records[i]
-        prokka_contig_record = correct_start_coords_prokka(prokka_contig_record_pre, prodigal_correction_dict,
+        global ref_prom_fp_dict
+        ref_prom_fp_dict = get_prom_for_gene(ref_features[i], ref_sequence)
+
+        prokka_contig_record = correct_start_coords_prokka(prokka_contig_record_pre, incorrect_coords_dict,
                                                            record_sequence, ref_sequence, ref_feature_dict,
                                                            reference_locus_list, reference_gene_locus_dict,
                                                            os.path.join(isolate_id, 'prokka','hybran_coord_corrections.tsv'))

commit 69b068137a9f669b65d62b7f9cc0ce91d155e347

annomerge: fix regression affecting non-1-to-1 inputs to reference
If the input genome has more sequences than the reference's, we'd get
an index error since the program was expecting them to have the same
number of sequences and that input_i would correspond to ref_i. This
was a regression introduced in
https://gitlab.com/LPCDRP/hybran/-/commit/a7aa1f24b2d94846a7afe67cf7c2ce625cb9901f

src/hybran/annomerge.py
@@ -246,7 +246,10 @@ def get_nuc_seq_for_gene(feature_list, source_seq):
             continue
         locus = f.qualifiers['locus_tag'][0]
         prom_seq = upstream_context(f.location, source_seq)
-        ref_fna_dict[locus] = SeqRecord(seq=f.extract(source_seq), id=locus)
+        ref_fna_dict[locus] = SeqRecord(
+            seq=f.extract(f.references[f.ref], references=f.references),
+            id=locus
+        )
         fp_prom = tempfile.NamedTemporaryFile(suffix='_prom.fasta',
                                               dir=hybran_tmp_dir,
                                               delete=False,
@@ -758,7 +761,7 @@ def coord_check(feature, ref_feature, fix_start=False, fix_stop=False, ref_gene_
     :return: True/False if the start/stop was fixed
     """
 
-    ref_seq = ref_feature.extract(ref_sequence)
+    ref_seq = ref_feature.extract(ref_feature.references[ref_feature.ref], references=ref_feature.references)
     ref_length = len(ref_seq)
     feature_start = int(feature.location.start)
     feature_end = int(feature.location.end)
@@ -1086,7 +1089,10 @@ def pseudoscan(feature, ref_feature, seq_ident, seq_covg, attempt_rescue=False, 
         while not confirmed_feature:
             good_start, good_stop = coord_check(feature, ref_feature, fix_start, fix_stop)
             coords_ok = [good_start, good_stop]
-            ref_seq = translate(ref_feature.extract(ref_sequence), table=genetic_code, to_stop=True)
+            ref_seq = translate(
+                ref_feature.extract(ref_feature.references[ref_feature.ref], references=ref_feature.references),
+                table=genetic_code, to_stop=True
+            )
             feature_seq = translate(feature.extract(record_sequence), table=genetic_code, to_stop=True)
             #ref_match with 'thresholds enforced'
             top_hit, low_covg, blast_stats = BLAST.reference_match(
@@ -1852,8 +1858,6 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_gbk_fp, refe
     corrected_ratt_orf_logfile = os.path.join(isolate_id, 'ratt', 'hybran_coord_corrections.tsv')
     global corrected_orf_report
     corrected_orf_report = []
-    ref_contigs = []
-    ref_features = []
     # create a dictionary of reference CDS annotations (needed for liftover to ab initio)
     global ref_annotation
     ref_annotation = {}
@@ -1862,25 +1866,25 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_gbk_fp, refe
     global ref_fna_dict
     ref_prom_fp_dict = {}
     ref_fna_dict = {}
+    ref_id = os.path.basename(os.path.splitext(ref_gbk_fp)[0])
     for ref_record in SeqIO.parse(ref_gbk_fp, 'genbank'):
-        ref_contigs.append(ref_record.seq)
-        ref_features.append(ref_record.features)
-        prom, fna = get_nuc_seq_for_gene(ref_record.features,ref_record.seq)
-        ref_prom_fp_dict.update(prom)
-        ref_fna_dict.update(fna)
+        ref_contig_id = '.'.join([ref_id, ref_record.name])
         for feature in ref_record.features:
             if feature.type != "CDS":
                 continue
+            feature.ref = ref_contig_id
+            feature.references = {ref_contig_id: ref_record.seq}
             # if reference paralogs have been collapsed, the last occurrence in the genome
             # will prevail.
             ref_annotation[feature.qualifiers['gene'][0]] = feature
+        prom, fna = get_nuc_seq_for_gene(ref_record.features,ref_record.seq)
+        ref_prom_fp_dict.update(prom)
+        ref_fna_dict.update(fna)
 
     output_isolate_recs = []
 
     for i, contig in enumerate(contigs):
         seqname = '.'.join([isolate_id, contig])
-        global ref_sequence
-        ref_sequence = ref_contigs[i]
         ratt_contig_record = SeqIO.read(ratt_gbk_files[contig], 'genbank')
         global record_sequence
         record_sequence = ratt_contig_record.seq
@@ -2078,7 +2082,7 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_gbk_fp, refe
                         feature_is_pseudo,
                         inference=':'.join([
                             f"similar to AA sequence",
-                            os.path.basename(os.path.splitext(ref_gbk_fp)[0]),
+                            ref_id,
                             ref_annotation[ref_gene].qualifiers['locus_tag'][0],
                             ref_gene,
                             "blastp",
@@ -2204,7 +2208,7 @@ def run(isolate_id, contigs, annotation_fp, ref_proteins_fasta, ref_gbk_fp, refe
                                 pseudo,
                                 inference=':'.join([
                                     "similar to AA sequence",
-                                    os.path.basename(os.path.splitext(ref_gbk_fp)[0]),
+                                    ref_id,
                                     ref_annotation[ref_gene].qualifiers['locus_tag'][0],
                                     ref_gene,
                                     "RATT+blastp",

tests/data_features.py
@@ -8,14 +8,21 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation, ExactPosition, CompoundL
 
 ref_features = {}
 for ref in [
-        'H37Rv', 'nissle-hybrid'
+        'H37Rv',
+        'nissle-hybrid',
 ]:
     ref_file = os.path.join(pathlib.Path(__file__).parent.resolve(), 'data', f'{ref}.gbk')
     ref_features[ref] = {}
     if os.path.isfile(ref_file):
         for record in SeqIO.parse(ref_file, 'genbank'):
+            k = '.'.join([
+                os.path.splitext(os.path.basename(ref_file))[0],
+                record.name,
+            ])
             for feature in record.features:
                 if 'gene' in feature.qualifiers:
+                    feature.ref = k
+                    feature.references = {k: record.seq}
                     ref_features[ref][feature.qualifiers['gene'][0]] = feature
 
 

tests/test_annomerge.py
@@ -196,7 +196,6 @@ def test_process_split_genes(gene_list):
     ref_genome = defaultdict(lambda :'H37Rv')
     source_genome = source_genomes[gene_list]
     annomerge.record_sequence = list(SeqIO.parse(f'data/{source_genome}.fasta', 'fasta'))[0].seq
-    annomerge.ref_sequence = SeqIO.read('data/H37Rv.fasta', 'fasta').seq
     annomerge.genetic_code = 11
     annomerge.ref_annotation = ref_features[ref_genome[gene_list]]
     annomerge.corrected_orf_report = []
@@ -395,8 +394,6 @@ def test_coord_check(feature_type, fix_start, fix_stop):
         test_features[feature_type].qualifiers['gene'][0]
     ]
     annomerge.record_sequence = list(SeqIO.parse(f'data/{source_genome[feature_type]}.fasta', 'fasta'))[0].seq
-    annomerge.ref_sequence = list(SeqIO.parse(f'data/{ref_genome[feature_type]}.gbk', 'genbank'))[0].seq
-
     annomerge.genetic_code = 11
     annomerge.corrected_orf_report = []
 
@@ -475,7 +472,6 @@ def test_pseudoscan(feature_type, seq_ident, seq_covg, attempt_rescue, tmp_path)
         test_features[feature_type].qualifiers['gene'][0]
     ]
     annomerge.record_sequence = list(SeqIO.parse(f'data/{source_genome[feature_type]}.fasta', 'fasta'))[0].seq
-    annomerge.ref_sequence = SeqIO.read('data/H37Rv.fasta', 'fasta').seq
     annomerge.genetic_code = 11
     annomerge.corrected_orf_report = []
     expected = {
@@ -635,7 +631,6 @@ def test_isolate_valid_ratt_annotations(case):
 
     annomerge.ref_annotation = ref_features[ref_genome[case]]
     annomerge.genetic_code = 11
-    annomerge.ref_sequence = SeqIO.read('data/H37Rv.fasta', 'fasta').seq
 
     ref_temp_fasta_dict = dict(
         Rv0001  = 'gene-seqs/Rv0001.fasta',
@@ -816,7 +811,6 @@ def test_check_inclusion_criteria(pair, tmp_path):
     abinit = features[source_genome[pair]][pairs[pair][1]]['abinit']
 
     annomerge.record_sequence = list(SeqIO.parse(f'data/{source_genome[pair]}.fasta', 'fasta'))[0].seq
-    annomerge.ref_sequence = SeqIO.read('data/H37Rv.fasta', 'fasta').seq
     annomerge.ref_annotation = ref_features[ref_genome[pair]]
     reference_gene_locus_dict = defaultdict(list, dict(
         dnaA=['Rv0001'],