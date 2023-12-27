f989dd41d08e889fb1c624735043c201bda3a07e
Afif Elghraoui <aelghraoui@sdsu.edu>
2023-12-27 02:07:17 -0800

    Add test case for compare() as currently written

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,4 +1,4 @@
-from Bio.SeqFeature import SeqFeature
+from Bio.SeqFeature import SeqFeature, FeatureLocation, ExactPosition
 import pytest
 
 from hybran import compare
@@ -41,3 +41,74 @@ def test_pgap_np(case):
     assert compare.pgap_np(
         SeqFeature(qualifiers={'note': [ note[case] ]})
     ) == expected[case]
+
+
+@pytest.mark.parametrize('case', [
+    'colo_conflict_fusion',
+])
+def test_compare(case):
+    inputs = {
+        'colo_conflict_fusion': [
+            [
+                # pgap
+                SeqFeature(
+                    FeatureLocation(959902, 960772, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_000905'], 'gene':['trxA']}
+                ),
+                SeqFeature(
+                    FeatureLocation(960843, 961506, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_000906']}
+                ),
+                SeqFeature(
+                    FeatureLocation(961502, 961973, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_000907']}
+                ),
+            ], [
+                # hybran
+                SeqFeature(
+                    FeatureLocation(959902, 960772, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_00926'], 'gene':['PA4061']}
+                ),
+                SeqFeature(
+                    FeatureLocation(960843, 961122, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_00927'], 'gene':['PA4060']}
+                ),
+                SeqFeature(
+                    FeatureLocation(960843, 961506, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_00928'], 'gene':['PA4059::PA4060']}
+                ),
+                SeqFeature(
+                    FeatureLocation(961502, 961973, -1), type='CDS',
+                    qualifiers={'locus_tag':['PAK_00929'], 'gene':['PA4058']}
+                ),
+            ]
+        ],
+    }
+    f1_list, f2_list = inputs[case]
+
+    expected = {
+        'colo_conflict_fusion': (
+            # colocated
+            [
+                [
+                    'PAK_000905', 'trxA', 0, '.',
+                    'PAK_00926', 'PA4061', 0, '.',
+                    ExactPosition(959902), ExactPosition(960772), -1
+                ], [
+                    'PAK_000906', 'PAK_000906', 0, '.',
+                    'PAK_00928', 'PA4059::PA4060', 0, '.',
+                    ExactPosition(960843), ExactPosition(961506),  -1
+                ], [
+                    'PAK_000907', 'PAK_000907', 0, '.',
+                    'PAK_00929', 'PA4058', 0, '.',
+                    ExactPosition(961502), ExactPosition(961973), -1
+                ],
+            ],
+            # conflicting
+            [],
+            # unique
+            []
+        ),
+    }
+
+    assert compare.compare(f1_list, f2_list) == expected[case]
