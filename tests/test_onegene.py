8607ee0be5b2b0212919e7599b00055a0581d795
Afif Elghraoui <aelghraoui@sdsu.edu>
2026-02-08 13:53:29 -0800

    test_onegene: update test cases for an earlier behavior change
    See commit 24675ed158f5fa639bec88f64b6ec30eb02d99e3 for said behavior
    change.

--- tests/test_onegene.py
+++ tests/test_onegene.py
@@ -15,29 +15,29 @@ def test_name_cluster(situation):
 
     inputs = {
         'singleref_multiple_nonames': [
-            'Ref1%%%Rv1369c%%%Rv1369c',
-            'Ref1%%%Rv1756c%%%Rv1756c',
-            'Ref1%%%Rv1764%%%Rv1764',
+            'Ref1.1%%%Rv1369c%%%Rv1369c',
+            'Ref1.1%%%Rv1756c%%%Rv1756c',
+            'Ref1.1%%%Rv1764%%%Rv1764',
         ],
         'singleref_one_named': [
-            'Ref1%%%Rv1454c%%%qor',
+            'Ref1.1%%%Rv1454c%%%qor',
         ],
         'multiref_single_orthologs': [
-            'Ref1%%%R1_01234%%%R1_01234',
-            'Ref2%%%R2_01234%%%R2_01234',
+            'Ref1.1%%%R1_01234%%%R1_01234',
+            'Ref2.1%%%R2_01234%%%R2_01234',
         ],
         'multiref_mixed_bag': [
-            'Ref1%%%R1_01234%%%R1_01234',
-            'Ref1%%%R1_05678%%%',
-            'Ref2%%%R2_01234%%%R2_01234',
-            'Ref3%%%R3_01234%%%genA',
-            'Ref3%%%R3_06789%%%R3_06789',
+            'Ref1.1%%%R1_01234%%%R1_01234',
+            'Ref1.1%%%R1_05678%%%',
+            'Ref2.1%%%R2_01234%%%R2_01234',
+            'Ref3.1%%%R3_01234%%%genA',
+            'Ref3.1%%%R3_06789%%%R3_06789',
         ],
         'multiref_orthologous_named_duplicates': [
-            'Ref1%%%R1_01234%%%genA',
-            'Ref1%%%R1_01235%%%genB',
-            'Ref2%%%R2_01234%%%genA',
-            'Ref2%%%R2_01235%%%genB',
+            'Ref1.1%%%R1_01234%%%genA',
+            'Ref1.1%%%R1_01235%%%genB',
+            'Ref2.1%%%R2_01234%%%genA',
+            'Ref2.1%%%R2_01235%%%genB',
         ],
     }
 
@@ -46,14 +46,14 @@ def test_name_cluster(situation):
     expected_subs = defaultdict(dict)
     expected = {
         'singleref_multiple_nonames': [
-            {'Ref1': {
+            {'Ref1.1': {
                 'Rv1369c':'REFORFX0001',
                 'Rv1756c':'REFORFX0001',
                 'Rv1764':'REFORFX0001',
             }},
-            ("Ref1\tRv1369c\tRv1369c\tREFORFX0001\n"
-             "Ref1\tRv1756c\tRv1756c\tREFORFX0001\n"
-             "Ref1\tRv1764\tRv1764\tREFORFX0001\n"),
+            ("Ref1.1\tRv1369c\tRv1369c\tREFORFX0001\n"
+             "Ref1.1\tRv1756c\tRv1756c\tREFORFX0001\n"
+             "Ref1.1\tRv1764\tRv1764\tREFORFX0001\n"),
             2,
         ],
         'singleref_one_named': [
@@ -62,52 +62,52 @@ def test_name_cluster(situation):
             1,
         ],
         'multiref_single_orthologs': [
-            {'Ref1': {
+            {'Ref1.1': {
                 'R1_01234':'R1_01234',
             },
-             'Ref2': {
+             'Ref2.1': {
                 'R2_01234':'R1_01234',
             },
             },
-            ("Ref1\tR1_01234\tR1_01234\tR1_01234\n"
-             "Ref2\tR2_01234\tR2_01234\tR1_01234\n"),
+            ("Ref1.1\tR1_01234\tR1_01234\tR1_01234\n"
+             "Ref2.1\tR2_01234\tR2_01234\tR1_01234\n"),
             1,
         ],
         'multiref_mixed_bag': [
-            {'Ref1': {
+            {'Ref1.1': {
                 'R1_01234':'genA',
                 'R1_05678':'genA',
             },
-             'Ref2': {
+             'Ref2.1': {
                  'R2_01234': 'genA',
              },
-             'Ref3': {
+             'Ref3.1': {
                  'R3_01234':'genA',
                  'R3_06789':'genA',
              },
              },
-            ("Ref1\tR1_01234\tR1_01234\tgenA\n"
-             "Ref1\tR1_05678\t\tgenA\n"
-             "Ref2\tR2_01234\tR2_01234\tgenA\n"
-             "Ref3\tR3_01234\tgenA\tgenA\n"
-             "Ref3\tR3_06789\tR3_06789\tgenA\n"
+            ("Ref1.1\tR1_01234\tR1_01234\tgenA\n"
+             "Ref1.1\tR1_05678\t\tgenA\n"
+             "Ref2.1\tR2_01234\tR2_01234\tgenA\n"
+             "Ref3.1\tR3_01234\tgenA\tgenA\n"
+             "Ref3.1\tR3_06789\tR3_06789\tgenA\n"
              ),
             1,
         ],
         'multiref_orthologous_named_duplicates': [
-            {'Ref1': {
+            {'Ref1.1': {
                 'R1_01234':'REFORFX0001',
                 'R1_01235':'REFORFX0001',
             },
-             'Ref2': {
+             'Ref2.1': {
                 'R2_01234':'REFORFX0001',
                 'R2_01235':'REFORFX0001',
              },
              },
-            ("Ref1\tR1_01234\tgenA\tREFORFX0001\n"
-             "Ref1\tR1_01235\tgenB\tREFORFX0001\n"
-             "Ref2\tR2_01234\tgenA\tREFORFX0001\n"
-             "Ref2\tR2_01235\tgenB\tREFORFX0001\n"
+            ("Ref1.1\tR1_01234\tgenA\tREFORFX0001\n"
+             "Ref1.1\tR1_01235\tgenB\tREFORFX0001\n"
+             "Ref2.1\tR2_01234\tgenA\tREFORFX0001\n"
+             "Ref2.1\tR2_01235\tgenB\tREFORFX0001\n"
              ),
             2,
         ],
